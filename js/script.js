de40c41947ea798eb926119991d0a94617921f23
Marilisa Moio <moiomarilisa@gmail.com>
2024-03-26 15:38:06 +0100

    feat: add bonus

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ createApp({
     data() {
         return {
             message: "weooo funziona?",
+            imgSource: "https://i.kym-cdn.com/photos/images/original/002/051/163/11a.gif",
         }
     }
 }).mount('#app')
